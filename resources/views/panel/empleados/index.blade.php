361c427873063a4c0f7f6e92dc5de442c0ee5f16
“Matias” <cisnerosmatias394@gmail.com>
2023-11-02 11:55:32 -0300

    Carga de modales

--- resources/views/panel/empleados/index.blade.php
+++ resources/views/panel/empleados/index.blade.php
@@ -44,9 +44,15 @@
                                     <td>{{ $empleado->fecha_ingreso_emp }}</td>
                                     <td>{{ $empleado->fecha_egreso_emp }}</td>
                                     <td>
-                                        <a class="btn btn-success btn-sm" href="{{ route('empleados.show', $empleado->legajo_emp) }}">Ver</a>
+                                        {{-- <a class="btn btn-success btn-sm" href="{{ route('empleados.show', $empleado->legajo_emp) }}">Ver</a> --}}
+                                        <button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#showModal" data-bs-emp="{{ $empleado }}">
+                                            Ver
+                                        </button>
                                         <a href="{{ route('empleados.edit', $empleado->legajo_emp) }}" class="btn btn-dark btn-sm">Editar</a>
-                                        <form action="{{ route('empleados.destroy', $empleado->legajo_emp) }}" method="POST">
+                                        {{-- <button type="button" class="btn btn-delete btn-sm btn-danger text-uppercase" data-toggle="modal" data-target="#deleteModal" data-id="{{ $empleado->legajo_emp }}" data-nombre="{{ $empleado->nombre_emp }}">
+                                            Eliminar
+                                        </button> --}}
+                                        <form action="{{ route('empleados.destroy', $empleado) }}" method="POST">
                                             @csrf @method('DELETE')
                                             <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
                                         </form>
@@ -55,6 +61,7 @@
                             @endforeach
                         </tbody>
                     </table>
+                    @include('panel.empleados.modals')
                 </div>
             </div>    
         </div>
@@ -81,4 +88,45 @@
 
     {{-- La funcion asset() es una funcion de Laravel PHP que nos dirige a la carpeta "public" --}}
     <script src="{{ asset('js/empleados.js') }}"></script>
+
+    <script>
+        $(document).ready(function () {
+            // Escucha el evento de apertura del modal
+            $('#showModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                var empleadoData = button.data('bs-emp');
+                
+                // Aquí puedes acceder a los datos del empleado y hacer lo que necesites
+                //console.log(empleadoData);
+    
+                // Puedes actualizar el contenido del modal con los datos del empleado
+                $('#modalTitle').text('Ficha de Empleado con Legajo #' + empleadoData.legajo_emp);
+                $('#nombre').text(empleadoData.nombre_emp);
+                $('#apellido').text(empleadoData.apellido_emp);
+                $('#dni').text(empleadoData.dni_emp);
+                $('#domicilio').text(empleadoData.domicilio_emp);
+                $('#telefono').text(empleadoData.telefono_emp);
+                $('#email').text(empleadoData.email_emp);
+                $('#tipo_emp').text(empleadoData.tipo_empleado.nombre_te);
+                $('#fecha_ingreso').text(empleadoData.fecha_ingreso_emp);
+                $('#fecha_egreso').text(empleadoData.fecha_egreso_emp);
+            });
+        });
+
+        $(document).ready(function(){
+
+            $('#deleteModal').on('show.bs.modal', function (event) {
+                const button = $(event.relatedTarget) // Button that triggered the modal
+                const id = button.data('id') // Extract info from data-* attributes
+                const nombre = button.data('nombre') // Extract info from data-* attributes
+                
+                const modal = $(this)
+                const form = $('#formDelete')
+                form.attr('action', `{{ url('empleados') }}/${id}`);
+
+                modal.find('.modal-body p#message').text(`¿Estás seguro de eliminar el producto "${nombre}"?`)
+            })
+        });
+    </script>
+    
 @stop
